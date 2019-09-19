b20f5afb6748817fdee43b7380771480f0d4867c
hannahw1ll1ams <hannahw1ll1ams@yahoo.com>
2019-09-19 14:03:11 +0100

    now the topic creator component is deleted

--- src/Components/ArticleCreator.js
+++ src/Components/ArticleCreator.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-// import TopicCreator from './TopicCreator';
 
 class ArticleCreator extends Component {
   state = {
--- src/Components/ViewToggler.js
+++ src/Components/ViewToggler.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import ArticleCreator from './ArticleCreator';
 import CommentCreator from './CommentCreator'
-import TopicCreator from './TopicCreator';
 
 class ViewToggler extends Component {
   state = {
@@ -22,7 +21,6 @@ class ViewToggler extends Component {
         </button>
         {(isShowing) && (item === 'article') && <ArticleCreator postNewArticle={postNewArticle} updateTopics={updateTopics} slugs={slugs} />}
         {(isShowing) && (item === 'comment') && <CommentCreator postNewComment={postNewComment} />}
-        {(isShowing) && (item === 'topic') && <TopicCreator updateTopics={updateTopics} />}
       </div>
     );
   }
