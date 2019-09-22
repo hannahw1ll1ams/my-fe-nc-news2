96dfa13f0a6423600031035fe4c6e384690d388f
hannahw1ll1ams <hannahw1ll1ams@yahoo.com>
2019-09-22 23:21:54 +0100

    basic positioning added to all users page

--- src/Components/AllUsersPage.js
+++ src/Components/AllUsersPage.js
@@ -1,28 +1,31 @@
 import React from 'react';
 import ErrorPage from './ErrorPage';
 import { Link } from '@reach/router'
+import '../css/allUsers.css'
 
 const UsersPage = ({ username, loggedInUser, users, isLoadingUsers, usersError }) => {
   let chosenUser = users.filter(users => users.username === username)
   return (
     <>
       {isLoadingUsers ? <p>LOADING USERS...</p> : usersError ? <ErrorPage error={usersError} /> :
-        <>{username === loggedInUser ? <p>YOU ARE {chosenUser[0].username}</p> : <p>INTRODUCING {chosenUser[0].name}</p>}
-
-          <img src={chosenUser[0].avatar_url} alt={chosenUser[0].name} />
-          <p>Username: {chosenUser[0].username}</p>
-          <Link to={`/articles/user/${chosenUser[0].username}`}><p>For more articles by {chosenUser[0].name}</p></Link>
-          <h1>OTHER USERS:</h1>
-
-          {users.map(user => {
-            return <li key={user.name}>
-              <Link to={`/users/${user.username}`}>
-                <img src={user.avatar_url} alt={user.name} />
-                <p>{user.username}</p>
-              </Link>
-            </li>
-          })
-          }</>
+        <>
+          <div className='loggedInUser'>{username === loggedInUser ? <p>YOU ARE {chosenUser[0].username}</p> : <p>INTRODUCING {chosenUser[0].name}</p>}
+            <img className='usersImg' src={chosenUser[0].avatar_url} alt={chosenUser[0].name} />
+            <p className='userName'>Username: {chosenUser[0].username}</p>
+            <Link to={`/articles/user/${chosenUser[0].username}`}><p>For more articles by {chosenUser[0].name}</p></Link>
+            <h1>OTHER USERS:</h1>
+          </div>
+          <ul className='otherUsersList'>
+            {users.map(user => {
+              return <li key={user.name} className='singleUser'>
+                <Link className='links' to={`/users/${user.username}`}>
+                  <img className='usersImg' src={user.avatar_url} alt={user.name} />
+                  <p>{user.username}</p>
+                </Link>
+              </li>
+            })
+            }
+          </ul></>
       }
     </>
   );
--- src/Views/UserByUsernamePage.js
+++ src/Views/UserByUsernamePage.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import AllUsersPage from '../Components/AllUsersPage';
+import '../css/allUsers.css'
 
 const UserByUsernamePage = ({ username, loggedInUser, users, usersError, isLoadingUsers }) => {
   return (
-    <>
+    <div className='allUsersPage'>
       {loggedInUser &&
         <AllUsersPage username={username} loggedInUser={loggedInUser} users={users} isLoadingUsers={isLoadingUsers} usersError={usersError} />
       }
-    </>
+    </div>
   );
 };
 
