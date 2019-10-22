2e545e3d4b622a6351857343e5613389abc38b4f
hannahw1ll1ams <hannahw1ll1ams@yahoo.com>
2019-10-22 15:32:07 +0100

    removed delete button from article list

--- src/Components/ArticleCard.js
+++ src/Components/ArticleCard.js
@@ -1,6 +1,6 @@
 import React from 'react';
 // import { Link } from '@reach/router';
-import DeleteButton from './DeleteButton';
+// import DeleteButton from './DeleteButton';
 import moment from 'moment'
 
 
@@ -11,10 +11,11 @@ const ArticleCard = ({ title, topic, author, created_at, article_id, votes, comm
     <li className={`singleArticle-${topic}`}>
       <p>Posted by {author} {moment(created_at).fromNow()}</p>
       <h2>{title}</h2>
-      {loggedInUser === author && <DeleteButton item="article" id={article_id} deleteElementByClick={deleteElementByClick} />}
-
-      {selectedArticleIdVotes === article_id ? <p>{votesCountChange ? Number(votes) + Number(votesCountChange) : votes} votes </p> : <p> {votes} votes</p>}
-      {selectedArticleIdComments === article_id ? <p>Comments: {commentCountChange ? Number(comment_count) + Number(commentCountChange) : comment_count}</p> : <p> {comment_count} comments</p>}
+      {/* {loggedInUser === author && <DeleteButton item="article" id={article_id} deleteElementByClick={deleteElementByClick} />} */}
+      <div className='votesAndCommentsStats'>
+        {selectedArticleIdVotes === article_id ? <p>{votesCountChange ? Number(votes) + Number(votesCountChange) : votes} votes </p> : <p> {votes} votes</p>}
+        {selectedArticleIdComments === article_id ? <p>Comments: {commentCountChange ? Number(comment_count) + Number(commentCountChange) : comment_count}</p> : <p> {comment_count} comments</p>}
+      </div>
     </li>
   );
 };
